f1f70a3bc039f56b3c5d167f57353481946e993c
docfr10 <docfr10@yandex.ru>
2023-02-16 11:33:44 +0300

    Added recyclerView

---
+++ app/src/main/java/com/example/lesson_recyclerview/RecyclerViewItem.kt
@@ -0,0 +1,31 @@
+package com.example.lesson_recyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewItem(private val names: List<String>) :
+    RecyclerView.Adapter<RecyclerViewItem.MyViewHolder>() {
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val largeTextView: TextView = itemView.findViewById(R.id.textViewLarge)
+        val smallTextView: TextView = itemView.findViewById(R.id.textViewSmall)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.recycler_view_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.largeTextView.text = names[position]
+        holder.smallTextView.text = "Some text"
+    }
+
+    override fun getItemCount(): Int {
+        return names.size
+    }
+}
